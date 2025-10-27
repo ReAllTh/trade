b7ac84776088d50997cd530046ae8ebff67fb57b
reallth <z1197487156@gmail.com>
2025-10-27 15:40:08 +0800

    feature(customer/CustomerMapper.java): add customer mapper
    Add customer mapper to manipulate the ‘t_customer’ table.
    
    Signed-off-by: reallth <z1197487156@gmail.com>

---
+++ customer/src/main/java/com/kaismemo/trade/mapper/CustomerMapper.java
@@ -0,0 +1,20 @@
+package com.kaismemo.trade.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kaismemo.trade.domain.po.CustomerPo;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * customer 的持久化层
+ * <p>
+ * 提供了将持久化对象持久化到数据库中的能力
+ *
+ * @author reallth
+ * @since 1.0.0
+ */
+@Mapper
+public interface CustomerMapper extends BaseMapper<CustomerPo> {
+    default void save(CustomerPo customerPo) {
+        this.insert(customerPo);
+    }
+}
