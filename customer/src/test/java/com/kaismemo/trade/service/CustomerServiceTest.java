340ac0aedadb678e3a9d3807b9f5a866d8806c2b
reallth <z1197487156@gmail.com>
2025-10-28 20:07:48 +0800

    test(customer/CustomerServiceTest.java): add customer service integration test
    Integration test of service is more valuable than unit test.
    
    Signed-off-by: reallth <z1197487156@gmail.com>

---
+++ customer/src/test/java/com/kaismemo/trade/service/CustomerServiceTest.java
@@ -0,0 +1,48 @@
+package com.kaismemo.trade.service;
+
+import com.baomidou.mybatisplus.test.autoconfigure.MybatisPlusTest;
+import com.kaismemo.trade.config.MyBatisPlusTestConfig;
+import com.kaismemo.trade.domain.bo.CustomerBo;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.Import;
+import org.springframework.dao.DuplicateKeyException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * CustomerService 的轻量级集成测试类
+ *
+ * @author reallth
+ * @since 1.0.0
+ */
+@MybatisPlusTest
+@Import({MyBatisPlusTestConfig.class, CustomerService.class})
+class CustomerServiceTest {
+    @Resource
+    private CustomerService customerService;
+
+    @Test
+    public void should_void_when_valid() {
+        CustomerBo validBo = new CustomerBo() {{
+            setName("Oguri Cap");
+            setEmail("oguri_cap@kasamutsu.com");
+            setGender("Female");
+            setCountry("Japan");
+        }};
+        customerService.signup(validBo);
+        // 此处应当查库确认记录存在，后续查询接口实现后连带测试
+    }
+
+    @Test
+    public void should_error_when_duplicate() {
+        CustomerBo validBo = new CustomerBo() {{
+            setName("Oguri Cap");
+            setEmail("oguri_cap@kasamutsu.com");
+            setGender("Female");
+            setCountry("Japan");
+        }};
+        customerService.signup(validBo);
+        assertThrows(DuplicateKeyException.class, () -> customerService.signup(validBo));
+    }
+}
