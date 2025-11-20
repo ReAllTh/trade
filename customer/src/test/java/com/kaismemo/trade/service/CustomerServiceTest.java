c0e41a43dd1f0212a134295b42c994bd0c01d098
reallth <z1197487156@gmail.com>
2025-11-20 15:33:36 +0800

    test(customer): add customer update test
    Signed-off-by: reallth <z1197487156@gmail.com>

--- customer/src/test/java/com/kaismemo/trade/service/CustomerServiceTest.java
+++ customer/src/test/java/com/kaismemo/trade/service/CustomerServiceTest.java
@@ -6,11 +6,13 @@ import com.kaismemo.trade.config.MyBatisPlusTestConfig;
 import com.kaismemo.trade.domain.bo.CustomerBo;
 import com.kaismemo.trade.domain.req.CustomerQueryReq;
 import com.kaismemo.trade.domain.vo.CustomerVo;
+import com.kaismemo.trade.exception.BusinessException;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.Import;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDate;
 
@@ -70,5 +72,40 @@ class CustomerServiceTest {
         }};
         customerService.signup(validBo);
         assertThrows(DuplicateKeyException.class, () -> customerService.signup(validBo));
+    }
+
+    @Test
+    @DisplayName("合法用户更新查询")
+    @Sql(scripts = "/sql/dummy_customers.sql")
+    public void should_void_when_update_valid() {
+        CustomerBo validBo = new CustomerBo() {{
+            setEmail("oguri_cap@kasamutsu.com");
+            setName("Oguri Chain");
+        }};
+        CustomerVo expected = new CustomerVo() {{
+            setName("Oguri Chain");
+            setEmail("oguri_cap@kasamutsu.com");
+            setGender("Female");
+            setCountry("Japan");
+            setSignupDate(LocalDate.parse("2025-02-15"));
+        }};
+        CustomerQueryReq queryReq = new CustomerQueryReq() {{
+            setEmail("oguri_cap@kasamutsu.com");
+            setPage(1);
+            setPageSize(10);
+        }};
+        customerService.updateByEmail(validBo);
+        CustomerVo actual = customerService.query(queryReq).getData().getFirst();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("非法用户更新查询")
+    public void should_error_when_update_invalid() {
+        CustomerBo invalidBo = new CustomerBo() {{
+            setEmail("goda_katsuhito@joho.com");
+            setName("Goda Complex");
+        }};
+        assertThrows(BusinessException.class, () -> customerService.updateByEmail(invalidBo));
     }
 }
